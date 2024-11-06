f62da0ec6711a402c783d9c130a84e9605e6026f
OLoghunov <loghunov1@gmail.com>
2024-11-07 00:04:07 +0300

    KF 1-d state

--- Kalman filter/KalmanFilter.py
+++ Kalman filter/KalmanFilter.py
@@ -1,20 +1,56 @@
 import numpy as np
+iX = 0
+iV = 1
+NUMVARS = iV + 1
+
 class KF:
     def __init__(self, initialX: float, 
                        initialV: float,
                        accelVariance: float) -> None:
         # Mean of state
-        self._x = np.array([initialX, initialV])
+        self._x = np.zeros(NUMVARS)
+        self._x[iX] = initialX
+        self._x[iV] = initialV
         
         self._accelVariance = accelVariance
+        
         # Covariance of state
-        self._P = np.eye(2) 
+        self._P = np.eye(NUMVARS) 
     
     def predict(self, dt: float) -> None:
-        F = np.array([[1, dt], [0, 1]])
+        # x = F*x
+        # P = F*P*F.T + G*G*T*a
+        F = np.eye(NUMVARS)
+        F[iX, iV] = dt
         newX = F.dot(self._x)
-        G = np.array([0.5 * dt**2, dt]).reshape((2, 1))
+        
+        G = np.zeros((2, 1))
+        G[iX] = 0.5 * dt**2
+        G[iV] = dt
         newP = F.dot(self._P).dot(F.T) + G.dot(G.T) * self._accelVariance
+        
+        self._x = newX
+        self._P = newP
+        
+    def correct(self, measedValue: float, measedVariance):
+        # y = z - H*x
+        # S = H*P*H.T + R
+        # K = P*S^(-1)
+        # x = x + K*y
+        # P = (I - K*H)*P
+        
+        z = np.array([measedValue])
+        R = np.array([measedVariance])
+        
+        H = np.zeros((1, NUMVARS))
+        H[0, iX] = 1
+        
+        y = z - H.dot(self._x)
+        S = H.dot(self._P).dot(H.T) + R
+        K = self._P.dot(H.T).dot(np.linalg.inv(S))
+        
+        newX = self._x + K.dot(y)
+        newP = (np.eye(2) - K.dot(H)).dot(self._P)
         
         self._x = newX
         self._P = newP
@@ -29,8 +65,8 @@ class KF:
         
     @property    
     def pos(self) -> float:
-        return self._x[0]
+        return self._x[iX]
     
     @property
     def vel(self) -> float:
-        return self._x[1]
+        return self._x[iV]
--- Kalman filter/main.py
+++ Kalman filter/main.py
@@ -24,42 +24,60 @@ for i in range(num_steps - 1):
     xyzs[i + 1] = numIntegrator(ode.integrate, xyzs[i], step)
 
 # Kalman filter usage
+realX = 0.0
+measVariance = 0.1 ** 2
+realV = 0.5
+
 kf = KF(0.0, 1.0, 1.5)
 dt = 0.1
 nSteps = 1000
+measEverySteps = 20
+
 mus = []
 covs = []
+realXs = []
+realVs = []
 
-for i in range(nSteps):
-    mus.append(kf.mean)
-    covs.append(kf.cov)
-    kf.predict(dt)
+# Инициализация фильтра Калмана
+kf = KF(initialX=0.1, initialV=0.0, accelVariance=1.0)
 
+# Фильтрация временного ряда
+filtered_states = []
+
+for measurement in noisyData:
+    kf.predict(dt=ode.stepSize())  # Предсказание (dt=1, измените при необходимости)
+    kf.correct(measedValue=measurement[0], measedVariance=0.01)  # Используем только первое значение для корректировки
+    filtered_states.append(kf.mean)
+
+filtered_states = np.array(filtered_states)
 
 # Plot
-# ax = plt.figure().add_subplot(projection='3d')
+ax = plt.figure().add_subplot(projection='3d')
 
-# ax.plot(*noisyData.T, lw=0.5)
-# ax.plot(*xyzs.T, lw=0.5)
-# ax.set_xlabel("X Axis")
-# ax.set_ylabel("Y Axis")
-# ax.set_zlabel("Z Axis")
+ax.plot(*noisyData.T, lw=0.5)
+#ax.plot(*xyzs.T, lw=0.5)
+ax.plot(*filtered_states.T, lw=0.5)
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
 
-# if isinstance(ode, Sprott):
-#     ax.set_title("Sprott Case I Attractor") 
-# elif isinstance(ode, Lorenz):
-#     ax.set_title("Lorenz Attractor") 
+if isinstance(ode, Sprott):
+    ax.set_title("Sprott Case I Attractor") 
+elif isinstance(ode, Lorenz):
+    ax.set_title("Lorenz Attractor") 
 
-plt.subplot(2,1,1)
-plt.title("Position")
-plt.plot([mu[0] for mu in mus])
-plt.plot([mu[0] - 2 * np.sqrt(cov[0,0]) for mu, cov in zip(mus,covs)], "--r")
-plt.plot([mu[0] + 2 * np.sqrt(cov[0,0]) for mu, cov in zip(mus,covs)], "--r")
+# plt.subplot(2,1,1)
+# plt.title("Position")
+# plt.plot([mu[0] for mu in mus])
+# plt.plot(realXs)
+# plt.plot([mu[0] - 2 * np.sqrt(cov[0,0]) for mu, cov in zip(mus,covs)], "--r")
+# plt.plot([mu[0] + 2 * np.sqrt(cov[0,0]) for mu, cov in zip(mus,covs)], "--r")
 
-plt.subplot(2,1,2)
-plt.title("Velocity")
-plt.plot([mu[1] for mu in mus])
-plt.plot([mu[1] - 2 * np.sqrt(cov[1,1]) for mu, cov in zip(mus,covs)], "--r")
-plt.plot([mu[1] + 2 * np.sqrt(cov[1,1]) for mu, cov in zip(mus,covs)], "--r")
+# plt.subplot(2,1,2)
+# plt.title("Velocity")
+# plt.plot([mu[1] for mu in mus])
+# plt.plot(realVs)
+# plt.plot([mu[1] - 2 * np.sqrt(cov[1,1]) for mu, cov in zip(mus,covs)], "--r")
+# plt.plot([mu[1] + 2 * np.sqrt(cov[1,1]) for mu, cov in zip(mus,covs)], "--r")
 
 plt.show()
