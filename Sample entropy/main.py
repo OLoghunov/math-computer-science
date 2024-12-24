33055ddee24322bc75c74b1de3c23cf30f5f2c25
OLoghunov <loghunov1@gmail.com>
2024-12-25 02:09:19 +0300

    Visualization of the m change

--- Sample entropy/main.py
+++ Sample entropy/main.py
@@ -5,7 +5,6 @@ import time
 from sampEn import *
 from view import view
 
-
 def main():
     paths = [
         "Sample entropy/data/1.xlsx",
@@ -14,31 +13,28 @@ def main():
     ]
 
     # SampEn parameters
-    epsRange = np.arange(0.1, 1.1, 0.005)
-    m = 2
+    epsRange = np.arange(0.1, 1.1, 0.05)
+    mRange = range(1, 42, 10)
 
-    entropies = np.zeros(shape=(3, len(epsRange)))
+    entropies = np.zeros((3, len(mRange), len(epsRange)))
 
     for i, path in enumerate(paths):
 
-        # We read every fifth entry
+        # We read every fifth entry 
         data = pd.read_excel(
-            path, header=None, usecols=[2], skiprows=lambda x: x % 5 != 0
+            path, header=None, nrows=2000, usecols=[2], skiprows=lambda x: x % 5 != 0
         ).to_numpy()
 
-        xrange = []
-        
-        start_time = time.time()
+        for k, m in enumerate(mRange):
+            start_time = time.time()
 
-        for j, eps in enumerate(epsRange):
-            entropy = sampleEntropy(data, m, eps)
-            entropies[i][j] = entropy
-            xrange.append(eps)
+            for j, eps in enumerate(epsRange):
+                entropy = sampleEntropy(data, m, eps)
+                entropies[i, k, j] = entropy
 
-        print("--- %s seconds ---" % (time.time() - start_time))
+            print(f"File {i+1}.xlsx,\t m={m},\t estimation time: {round(time.time() - start_time, 2)} seconds")
 
-    view(entropies, xrange)
-
+    view(entropies, epsRange, mRange)
 
 if __name__ == "__main__":
     print("Entropy estimation")
--- Sample entropy/view.py
+++ Sample entropy/view.py
@@ -1,20 +1,31 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-
-def view(data, xrange):
+def view(data, xrange, m_values):
     fig = plt.figure()
+    colors = plt.cm.plasma(np.linspace(0, 1, len(m_values)))
+
     ax1 = fig.add_subplot(311)
-    ax1.plot(xrange, data[0])
+    for i, m in enumerate(m_values):
+        ax1.plot(xrange, data[0, i, :], label=f"m={m}", color=colors[i])
     ax1.set_ylabel("SampEn")
-    
+    ax1.legend()
+    ax1.grid()
+
     ax2 = fig.add_subplot(312)
-    ax2.plot(xrange, data[1])
+    for i, m in enumerate(m_values):
+        ax2.plot(xrange, data[1, i, :], label=f"m={m}", color=colors[i])
     ax2.set_ylabel("SampEn")
+    ax2.legend()
+    ax2.grid()
 
     ax3 = fig.add_subplot(313)
-    ax3.plot(xrange, data[2])
+    for i, m in enumerate(m_values):
+        ax3.plot(xrange, data[2, i, :], label=f"m={m}", color=colors[i])
     ax3.set_ylabel("SampEn")
     ax3.set_xlabel("r")
+    ax3.legend()
+    ax3.grid()
 
     plt.tight_layout()
     plt.show()
